16776d22a12dd4f648b48cd9cfc21ce2d342620b
Fran Hermoso <franhp@gmail.com>
2011-10-12 23:35:15 +0200

    xmpp standarization

---
+++ servidor/src/xinxat/server/Roster.java
@@ -0,0 +1,14 @@
+/* Roster
+
+Roster is the name given to the list of contacts which appears in one's chat client.
+ In Google Talk terms, it is the list of friends you would see once you login.
+  The elements of the roster are your contacts. Each of these items is identified by 
+  a unique identifier called JID (Jabber Identifier). JIDs have a syntax similar to that 
+  of Email addresses, i.e., user@domain. The domain part in this identifier is usually 
+  the server one connects to. In case of Google Talk, it is gmail.com or any of its domain aliases.
+
+Rosters are stored on the server so that the XMPP client may retrieve the same each
+ time the user logs in. Rosters are a type of IQ Stanza. (See IQ explanation.) 
+
+*/
+
