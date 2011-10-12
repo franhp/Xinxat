aca7a35c618d86d85dff1d2d66f4d8bc8469119b
Fran Hermoso <franhp@gmail.com>
2011-10-12 23:35:29 +0200

    really xmpp standarization

--- servidor/src/xinxat/server/Server.java
+++ servidor/src/xinxat/server/Server.java
@@ -16,22 +16,32 @@ import com.google.appengine.api.xmpp.XMPPServiceFactory;
 
 public class Server extends HttpServlet {
 	
-	private Stack<String> pila = new Stack<String>();
+	private Stack<Message> pila = new Stack<Message>();
 	
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
-		
-		XMPPService xmpp = XMPPServiceFactory.getXMPPService();
-	    Message msg = xmpp.parseMessage(req.getParameter("msg"));
-	    
-	    JID fromJid = msg.getFromJid();
-	    JID toJid = msg.getRecipientJids();
-	    String body = msg.getBody();
+
+	    JID fromJid = new JID(req.getParameter("from"));
+	    JID recipientJids = new JID(req.getParameter("to"));
+	    String body = req.getParameter("msg");
+    	Message message = new MessageBuilder().withRecipientJids(recipientJids).withFromJid(fromJid).withBody(body).build();
     	
-    	Message missatge = new MessageBuilder().withFromJid(fromJid).withRecipient(toJid).withBody(body).build();
     	
-    	pila.push(missatge.toString());
+    	pila.push(message);
+    	
+    	
     	resp.getWriter().println("OK");
+    	
+    	/* Stanza?
+    	 <presence 
+    	 	from="romeo@gmail.com" 
+    	 	to="juliet@gmail.com" 
+    	 	xml:lang="en"> 
+    	 	<show>dnd</show> 
+    	 	<status>Good night, good night! parting is such sweet sorrow, 
+    	 	that I shall say good night till it be morrow.</status> 
+    	 </presence>
+    	 */
     }
     
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -42,8 +52,18 @@ public class Server extends HttpServlet {
     	if(pila.isEmpty()) resp.getWriter().println("EMPTY");
 	    else {
 	    	while (!pila.isEmpty()){
-	    		String missatge = pila.pop();
-	    		resp.getWriter().println(missatge);
+	    		Message missatge = pila.pop();
+	    		
+	    		resp.getWriter().println("<message ");
+	    		resp.getWriter().println("from=" + missatge.getFromJid());
+	    		
+	    		for (JID forJID : missatge.getRecipientJids()) {
+	    			String recipientJid = forJID.toString();
+	    			resp.getWriter().println("to=" + recipientJid); 
+	    		}
+	    		resp.getWriter().println("> <body>" + missatge.getBody() + "</body>");
+	    		resp.getWriter().println("</message>");
+	    		
 	    	}
 	    }
     }
