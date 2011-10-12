8cfc2ba4547f6119e0c60935fe3354f8423d1c6a
gango89 <isaacgango@gmail.com>
2011-10-12 15:20:31 +0200

    Documentació:
    Afegir:
    · bdmysql.png
    · xinxat_mobil.png
    · Pressupost_v2_1.odt
    · Pressupost_v2_2.odt
    · Protocols.odt
    · avaluació_de_grup.odt
    
    Signed-off-by: gango89 <isaacgango@gmail.com>

--- database/main.sql
+++ database/main.sql
@@ -3,92 +3,161 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Temps de generaciŒ·ŒıŒÂ: 01-10-2011 a les 19:45:21
--- VersiŒ·ŒıŒÂ del servidor: 5.5.8
--- VersiŒ·ŒıŒÂ de PHP : 5.3.5
+-- Tiempo de generaciÛn: 12-10-2011 a las 15:13:04
+-- VersiÛn del servidor: 5.5.8
+-- VersiÛn de PHP: 5.3.5
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
 --
--- Base de dades: `framework`
+-- Base de datos: `xinxat`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de la taula `categories`
+-- Estructura de tabla para la tabla `access`
+--
+
+CREATE TABLE IF NOT EXISTS `access` (
+  `id_access` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `id_room` int(11) NOT NULL,
+  `state` smallint(1) NOT NULL,
+  `role` smallint(1) NOT NULL,
+  PRIMARY KEY (`id_access`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `access`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categories`
 --
 
 CREATE TABLE IF NOT EXISTS `categories` (
- `id` int(11) NOT NULL DEFAULT '0',
+  `id` int(11) NOT NULL DEFAULT '0',
   `category_name` varchar(50) DEFAULT NULL,
-   PRIMARY KEY (`id`)
-   ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-   -- --------------------------------------------------------
+--
+-- Volcar la base de datos para la tabla `categories`
+--
 
-   --
-   -- Estructura de la taula `comments`
-   --
 
-   CREATE TABLE IF NOT EXISTS `comments` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-     `userid` int(11) NOT NULL,
-      `postid` int(11) NOT NULL,
-       `comment` varchar(1000) NOT NULL,
-        `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	 PRIMARY KEY (`id`)
-	 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
+-- --------------------------------------------------------
 
-	 -- --------------------------------------------------------
+--
+-- Estructura de tabla para la tabla `comments`
+--
 
-	 --
-	 -- Estructura de la taula `posts`
-	 --
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `postid` int(11) NOT NULL,
+  `comment` varchar(1000) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-	 CREATE TABLE IF NOT EXISTS `posts` (
-	  `id` int(11) NOT NULL AUTO_INCREMENT,
-	   `title` varchar(100) NOT NULL,
-	    `body` varchar(5000) NOT NULL,
-	     `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	      `userid` int(11) NOT NULL,
-	       `seourl` varchar(50) NOT NULL,
-	        PRIMARY KEY (`id`),
-		 KEY `orderByDate` (`date`)
-		 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
+--
+-- Volcar la base de datos para la tabla `comments`
+--
 
-		 -- --------------------------------------------------------
 
-		 --
-		 -- Estructura de la taula `posts_categories`
-		 --
+-- --------------------------------------------------------
 
-		 CREATE TABLE IF NOT EXISTS `posts_categories` (
-		  `id` int(11) NOT NULL DEFAULT '0',
-		   `id_post` int(11) DEFAULT NULL,
-		    `id_category` int(11) DEFAULT NULL,
-		     PRIMARY KEY (`id`)
-		     ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Estructura de tabla para la tabla `posts`
+--
 
-		     -- --------------------------------------------------------
-		     --
-		     -- Estructura de la taula `users`
-		     --
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `body` varchar(5000) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `userid` int(11) NOT NULL,
+  `seourl` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `orderByDate` (`date`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-		     CREATE TABLE IF NOT EXISTS `users` (
-		      `id` int(11) NOT NULL AUTO_INCREMENT,
-		       `username` varchar(25) NOT NULL,
-		        `password` varchar(50) NOT NULL,
-			 `name` varchar(100) NOT NULL,
-			  `lastname` varchar(100) NOT NULL,
-			  `lastlogin` varchar(100) NOT NULL,
-			  `lastonline` varchar(100) NOT NULL,
-			   `IP` varchar(10) NOT NULL,
-			    `location` varchar(50) NOT NULL,
-			     `birthdate` date NOT NULL,
-			      `email` varchar(50) NOT NULL,
-			       `role` int(11) NOT NULL DEFAULT '1',
-			       `oauth_token` varchar(50),
-			        PRIMARY KEY (`id`)
-				) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
+--
+-- Volcar la base de datos para la tabla `posts`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `posts_categories`
+--
+
+CREATE TABLE IF NOT EXISTS `posts_categories` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `id_post` int(11) DEFAULT NULL,
+  `id_category` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcar la base de datos para la tabla `posts_categories`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rooms`
+--
+
+CREATE TABLE IF NOT EXISTS `rooms` (
+  `id_room` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_room`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `rooms`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `lastname` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `lastlogin` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `lastonline` varchar(100) NOT NULL,
+  `IP` varchar(10) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `birthdate` date NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `role` int(11) NOT NULL DEFAULT '1',
+  `oauth_token` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `users`
+--
 
