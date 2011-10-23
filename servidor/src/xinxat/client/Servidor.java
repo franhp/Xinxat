6178726894aeade3c1e61055f5ff4121b994c9c4
Fran Hermoso <franhp@gmail.com>
2011-10-23 03:22:46 +0200

    fix so the project is deployable

---
+++ servidor/src/xinxat/client/Servidor.java
@@ -0,0 +1,16 @@
+package xinxat.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Servidor implements EntryPoint {
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+	}
+}
